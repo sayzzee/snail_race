6f7e943de621b74fbf265b371e6da32587a97cb8
sayzzee <steam.ivanov@bk.ru>
2023-11-07 23:35:41 +0300

    Добавление игрового поля (Class Field)

---
+++ ground.py
@@ -0,0 +1,19 @@
+import random
+
+FIELD = [['cucumber', 'peas', 'carrot', 'pepper', 'onion', 'tomato'],
+         ['pepper', 'onion', 'cucumber', 'peas', 'tomato', 'carrot'],
+         ['peas', 'tomato', 'pepper', 'carrot', 'cucumber', 'onion'],
+         ['cucumber', 'pepper', 'onion', 'carrot', 'peas', 'tomato']]
+
+class Field:
+
+    def __init__(self, FIELD):
+        for i in range(4):
+            side = random.randrange(-1, 2, 2)
+            FIELD[i] = FIELD[i][::side]
+            random.shuffle(FIELD)
+        self.field = FIELD
+        print(self.field)
+
+
+x = Field(FIELD)
